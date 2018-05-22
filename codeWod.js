3e43141c1cf0bc6052a427d5a31f39af37c4fc63
Jereme <jereme.daniels@gmail.com>
2018-05-22 15:33:16 -0400

    made a lot of progress in styling. added a blog which I thought was going to be hard.  Took 5 minutes

--- codeWod.js
+++ codeWod.js
@@ -1,11 +1,14 @@
 "use strict";
 
+var wodTogCount = 2;
 var currentResult = "<iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vTlUkyKqM_UBrIxv-7zqr-xQbmIhRwyfUJG6qKMYCGnDxgLPKKKaIqaJvB3bkeCdov6M4_M3nEV2xbQ/pubhtml?widget=true&amp;headers=false\"></iframe>" ;
 $(document).ready(function () {
      hideDivs();
      $("#wod").show();
      $("#workOutOfTheDayToggle").click(function(){
          $("#wod").toggle("slow");
+         wodArrows();
+        
      })
      $("#about").click(function(){
         hideDivs();
@@ -25,11 +28,17 @@ $(document).ready(function () {
          hideDivs();
          dropDown();
          $("#wod").show();
+         wodTogCount ++;
+         wodArrows();
          
      })
      $("#contact").click(function(){
         $("#contactInfo").toggle();
         dropDown();
+     })
+     $("#blogger").click(function(){
+         $("#blog").toggle();
+         dropDown();
      })
 });
 function dropDown() {
@@ -40,4 +49,15 @@ function hideDivs(){
     $("#information").hide();
     $("#resultDiv").hide();
     $("#contactInfo").hide();
+    $("#blog").hide();
+}
+function wodArrows(){
+    var wodTogUp = "<i class=\"fas fa-angle-double-down fa-2x\"></i>"
+    var wodTogDown = "<i class=\"fas fa-angle-double-up fa-2x\"></i>"
+    if (wodTogCount % 2 == 1) {
+        $("#workOutOfTheDayToggle").html(wodTogDown);
+    } else {
+        $("#workOutOfTheDayToggle").html(wodTogUp);
+    }
+    wodTogCount++;
 }
