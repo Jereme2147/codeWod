53d184ccc0a32b80544c69cbc2daf9b473b7392e
Jereme <jereme.daniels@gmail.com>
2018-05-28 22:13:41 -0400

    a lot of added functionality

--- codeWod.js
+++ codeWod.js
@@ -1,46 +1,62 @@
 "use strict";
 
-var wodTogCount = 2;
-var currentResult = "<iframe src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vTlUkyKqM_UBrIxv-7zqr-xQbmIhRwyfUJG6qKMYCGnDxgLPKKKaIqaJvB3bkeCdov6M4_M3nEV2xbQ/pubhtml?widget=true&amp;headers=false\"></iframe>" ;
-$(document).ready(function () {
-     hideDivs();
-     $("#wod").show();
-     $("#workOutOfTheDayToggle").click(function(){
-         $("#wod").toggle("slow");
-         wodArrows();
-        
-     })
-     $("#about").click(function(){
-        hideDivs();
-        $("#information").toggle("slow");
+$(document).ready(function(){
+    $("#blog").toggle();
+    $("#information").hide();
+    $("#wod").hide();
+    $("#workOutOfTheDayToggle").click(function(){
+        $("#wod").toggle("slow");
+        $("#crossDiv").toggle();
+    })
+    $("#cross").click(function(){
+        $("#wod").toggle("slow");
+        $("#crossDiv").toggle();
+    })
+    $("#currentWod").click(function(){
+        $("#wod").toggle("slow");
+        $("#crossDiv").toggle();
         dropDown();
-     })
-     $("#currentWod").click(function(){
-         hideDivs();
-         $("#wod").show();
-         dropDown();
-     })
-     $("#currentResults").click(function(){
+    })
+    $("#resultToggle").click(function(){
         $("#resultDiv").toggle();
+    })
+    $("#blogToggle").click(function(){
+        $("#blog").toggle();
+    })
+    $("#infoToggle").click(function(){
+        $("#information").toggle();
+    })
+    $("#about").click(function () {
+        $("#information").toggle();
+        dropDown();
+    })
+    $("#currentResults").click(function () {
+        $("#resultDiv").toggle();
+        dropDown();
+    })
+     $("#blogger").click(function () {
+        $("#blog").toggle();
+        dropDown(); 
+     })
+     $("#contact").click(function(){
          dropDown();
      })
      $("#reset").click(function(){
          hideDivs();
          dropDown();
-         $("#wod").show();
-         wodTogCount ++;
-         wodArrows();
-         
      })
-     $("#contact").click(function(){
-        $("#contactInfo").toggle();
-        dropDown();
+     $("#fullMenuHome").click(function(){
+         hideDivs();
      })
-     $("#blogger").click(function(){
-         $("#blog").toggle();
-         dropDown();
-     })
-});
+    $("#fullMenuBlog").click(function () {
+        $("#blog").toggle();
+    })
+    $("#fullMenuAbout").click(function () {
+        $("#information").toggle();
+    })
+    
+})
+
 function dropDown() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -48,16 +64,7 @@ function hideDivs(){
     //$("#wod").hide();
     $("#information").hide();
     $("#resultDiv").hide();
-    $("#contactInfo").hide();
     $("#blog").hide();
-}
-function wodArrows(){
-    var wodTogUp = "<i class=\"fas fa-angle-double-down fa-2x\"></i>"
-    var wodTogDown = "<i class=\"fas fa-angle-double-up fa-2x\"></i>"
-    if (wodTogCount % 2 == 1) {
-        $("#workOutOfTheDayToggle").html(wodTogDown);
-    } else {
-        $("#workOutOfTheDayToggle").html(wodTogUp);
-    }
-    wodTogCount++;
+    $("#wod").hide();
+    $("#crossDiv").show();
 }
