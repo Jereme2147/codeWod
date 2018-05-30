991f0bb9a8f2195f4e0a1b28de101763e8e027e5
Jereme <jereme.daniels@gmail.com>
2018-05-30 13:29:27 -0400

    might go live tomorrow with select fd memebers. Seems reasonable on most screens, there's a few issuesremaining.

--- codeWod.js
+++ codeWod.js
@@ -54,7 +54,9 @@ $(document).ready(function(){
     $("#fullMenuAbout").click(function () {
         $("#information").toggle();
     })
-    
+    $("#previousWorkouts").click(function(){
+        $("#historical").toggle();
+    })
 })
 
 function dropDown() {
@@ -67,4 +69,29 @@ function hideDivs(){
     $("#blog").hide();
     $("#wod").hide();
     $("#crossDiv").show();
+    $("#historical").hide();
 }
+
+
+// Drive api*************************************
+/* function start() {
+    // 2. Initialize the JavaScript client library.
+    gapi.client.init({
+        'apiKey': 'YOUR_API_KEY',
+        // clientId and scope are optional if auth is not required.
+        'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+        'scope': 'profile',
+    }).then(function () {
+        // 3. Initialize and make the API request.
+        return gapi.client.request({
+            'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+        })
+    }).then(function (response) {
+        console.log(response.result);
+    }, function (reason) {
+        console.log('Error: ' + reason.result.error.message);
+    });
+};
+// 1. Load the JavaScript client library.
+gapi.load('client', start); */
+
